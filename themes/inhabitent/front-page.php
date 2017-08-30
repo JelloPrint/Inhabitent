ae43a80fa4ebf9671215d466e2ae6e2b86db96ca
Matthew Cuthbert <matthew.cuthbert@live.com>
2017-08-30 05:17:15 -0700

    added functionality to front page as well style shop container and latest jounral posts

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -17,41 +17,69 @@ get_header(); ?>
 
     <!-- SHOP STUFF  -->
 
+    <section class="shop-stuff container">
+        <h2>Shop Stuff</h2>
+            <div class="product-type-blocks">
+                <?php 
+                    $args = array( 'post_type' => 'product-type' );
+                    $terms = get_terms( $args );
+                ?>
+
+                <?php foreach ($terms as $term): ; ?>
+                    <div class="product-type-block-wrapper">
+                        <img src="<?php echo get_site_url(); ?>/wp-content/themes/inhabitent/images/product-type-icons/<?php echo $term->slug?>.svg"/>
+                            <p><?php echo $term->description; ?></p>
+                            <div class="shop-btn">
+                                <a href="<?php echo get_site_url(); ?>/<?php echo $term->taxonomy; ?>/<?php echo $term->slug; ?>"> <?php echo $term->name; ?> Stuff</a>
+                            </div><!-- .shop-btn -->
+                        </div><!-- .product-type-block-wrapper -->
+                    <?php endforeach; ?>
+        </div><!-- .product-type-blocks -->
+    </section><!-- shop-stuff .container -->
+
 
     <!-- Latest JOURNAL POSTS -->
-    <section class="latest-journal-entries container">
-      <h2>inhabitent journal</h2>
-      
-        <?php
-            $args = array(
-              'post_type'   => 'post',
-              'order'       => 'DESC',
-              'numberposts' => 3,
-              'orderby'     => 'date',
-            );
-            $journal_posts = get_posts( $args ); // returns an array of posts
-        ?>
+    <section class="latest-entries container">
+        <h2>Inhabitent Journal</h2>
+        <div class="recent-journals">
 
-        <?php foreach ($journal_posts as $post) : setup_postdata( $post );?>
+            <?php /* Start the Loop */ ?>
+            <?php
+                $args = array( 
+                'post_type' => 'post',
+                'order' => 'DESC', 
+                'posts_per_page' => 3, 
+                'orderby' => 'date' 
+                );
 
-        <div class="latest-journal-entry">
-          <div class="container">
-            <?php if (has_post_thumbnail()) : ?>
-              <?php the_post_thumbnail( 'medium' ); ?></a>
-            <?php endif; ?>
-          </div> <!--#Container -->
+                $journal_posts = get_posts( $args ); // returns an array of posts
+                ?>
 
-        <div class="entry-wrapper">
-            <div class="entry-meta">
-                <?php inhabitent_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php inhabitent_posted_by(); ?>
-            </div> <!--#entry-meta -->
-            <h3><a class="Journal-entry-title" href="<? echo get_post_permalink() ?>"><?php the_title(); ?></a><h3>
-            </div> <!-- entry-wrapper -->
-            <a class="black-btn" href="<?echo get_post_permalink()?>">Read Entry</a>
-        </div> <!--#latest-journal-entry -->
+                <?php foreach ( $journal_posts as $post ) : setup_postdata( $post ); ?>
 
-        <?php endforeach; wp_reset_postdata(); ?>
-    </section> <!--#latest-journal-entries container"-->
+                <div class="recent-journal-block-item">
+                    <div class="thumbnail-wrapper">
+                        <?php if ( has_post_thumbnail() ) : ?>
+                        <?php the_post_thumbnail( 'large' ); ?>
+                        <?php endif; ?><!-- has_post_thumbnail -->         
+                    </div><!-- .thumbnail-wrapper -->
+
+                    <div class="entry-wrapper">
+                        <div class="entry-meta">
+                            <?php inhabitent_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?>
+                        </div><!-- .entry-meta -->
+                        <h3><a class="journal-entry-title" href="<?php echo get_post_permalink() ?>"><?php the_title(); ?></a></h3>
+                    </div><!-- .entry-wrapper -->          
+                    <div class="front-black-btn"> 
+                        <href="<?php echo get_post_permalink()?>">Read Entry</a>
+                    </div><!--black-btn -->
+                </div><!-- .recent-journal-block-item -->
+
+                <?php endforeach; wp_reset_postdata(); ?>
+
+        </div><!-- .recent-journals -->
+    </section><!-- .latest-entries container -->
+
 
     <!--Adventures POSTS--> 
     <section class="adventures-container">
@@ -87,7 +115,7 @@ get_header(); ?>
                     </div>
                 </div>
                 <p>
-                    <a>More Adventures</a>
+                    <a class="btn">More Adventures</a>
                 </p>
     </section> 
 
