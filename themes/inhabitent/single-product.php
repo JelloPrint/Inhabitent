7bec0bc6af32a3ae5313bdcd03fc65d084d3ba5d
Matthew Cuthbert <matthew.cuthbert@live.com>
2017-08-29 23:18:52 -0700

    worked on styling for single product page, fixed title-entry on about page

--- themes/inhabitent/single-product.php
+++ themes/inhabitent/single-product.php
@@ -11,30 +11,28 @@ get_header(); ?>
   <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
 
-
-      <div class="container">
         <?php while ( have_posts() ) : the_post(); ?>
 
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-          <div class="product-image-container">
+          <div class="product-image-wrapper">
   
             <?php if ( has_post_thumbnail() ) : ?>
             <?php the_post_thumbnail( 'large' ); ?>
             <?php endif; ?>
-          </div> <!-- #product-image-container" -->
+          </div> <!-- #product-image-wrapper" -->
 
-        <div class="product-content-container">
+        <div class="product-content-wrapper">
           <header class="entry-header">
           <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
           </header><!-- .entry-header -->
 
         <div class="entry-content">
-          <h2 class="price"><?php echo CFS()->get( 'price' ); ?></h2>
+          <p class="price"><?php echo CFS()->get( 'price' ); ?></p>
           <?php the_content(); ?>
             <div class="social-media-buttons">
-              <button type="button" class="sm-button"><i class="fa fa-facebook"></i> Like</button>
-              <button type="button" class="sm-button"><i class="fa fa-twitter"></i> Tweet</button>
-              <button type="button" class="sm-button"><i class="fa fa-pinterest"></i> Pin</button>
+              <button type="button" class="black-btn"><i class="fa fa-facebook"></i> Like</button>
+              <button type="button" class="black-btn"><i class="fa fa-twitter"></i> Tweet</button>
+              <button type="button" class="black-btn"><i class="fa fa-pinterest"></i> Pin</button>
             </div> <!-- #social-media-buttons -->
 
           <?php
@@ -45,10 +43,9 @@ get_header(); ?>
             ?>
             <?php endwhile; // End of the loop. ?>
           </div> <!-- #entry-content -->
-        </div> <!-- #product-content-container -->
+        </div> <!-- #product-content-wrapper -->
       </article> <!-- #post -->
 
-    </div> <!-- #container -->
   </main> <!-- #main -->
 </div> <!-- #primary -->
 
